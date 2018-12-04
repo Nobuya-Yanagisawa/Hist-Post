af5be0ec64e8c07a55ef769d8455a75d283850b8
Nobuya-Yanagisawa <yannob17@gmail.com>
2018-12-04 10:35:26 +0000

    Add Post model controller views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
+	//= require activestorage
+  //= require jquery
+	//= require bootstrap-sprockets
+	//= require turbolinks
+	//= require_tree .
