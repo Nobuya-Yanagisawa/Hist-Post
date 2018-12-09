b794d1f4cfbc7ee244ae68b3cd278b58e5453fed
Nobuya-Yanagisawa <yannob17@gmail.com>
2018-12-09 05:44:03 +0000

    Add devise function

--- db/migrate/20181204092854_devise_create_users.rb
+++ db/migrate/20181204092854_devise_create_users.rb
@@ -33,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
       t.string :user_name
-      t.boolean :admin, default: false
+      # t.boolean :admin, default: false
       t.timestamps null: false
     end
 
