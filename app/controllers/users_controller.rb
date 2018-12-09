9336015caf91791f266f31b2614395df915e01fa
Nobuya-Yanagisawa <yannob17@gmail.com>
2018-12-09 11:06:15 +0000

    Modify top page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,27 @@
 class UsersController < ApplicationController
+  before_action :admin_user, only: [:index]
+  before_action :correct_user, only: [:show, :edit, :update]
+
   def index
   	@users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      @user = User.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2018_12_04_094806) do
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "user_name"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
